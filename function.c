76961338f9c0ca542c9dba49b2cdc04542d2a6d0
Ashenoy64 <59566362+Ashenoy64@users.noreply.github.com>
2022-05-31 14:29:23 +0530

    Add files via upload

---
+++ function.c
@@ -0,0 +1,90 @@
+void ADD ()            //ADD
+{
+    uint16_t r0 = (instr >> 9) & 0x7;
+    uint16_t r1 = (instr >> 6) & 0x7;
+    uint16_t imm_flag = (instr >> 5) & 0x1;
+
+    if (imm_flag)
+    {
+        uint16_t imm5 = sign_extend(instr & 0x1F, 5);
+        reg[r0] = reg[r1] & imm5;
+    }
+    else
+    {
+        uint16_t r2 = instr & 0x7;
+        reg[r0] = reg[r1] & reg[r2];
+    }
+    update_flags(r0);
+}
+
+
+void AND()
+{
+    uint16_t r0 = (instr >> 9) & 0x7;
+    uint16_t r1 = (instr >> 6) & 0x7;
+    uint16_t imm_flag = (instr >> 5) & 0x1;
+
+    if (imm_flag)
+    {
+        uint16_t imm5 = sign_extend(instr & 0x1F, 5);
+        reg[r0] = reg[r1] & imm5;
+    }
+    else
+    {
+        uint16_t r2 = instr & 0x7;
+        reg[r0] = reg[r1] & reg[r2];
+    }
+    update_flags(r0);
+}
+
+void NOT()
+{
+    uint16_t r0 = (instr >> 9) & 0x7;
+    uint16_t r1 = (instr >> 6) & 0x7;
+
+    reg[r0] = ~reg[r1];
+    update_flags(r0);
+}
+
+
+void BRANCH()
+{
+    uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+    uint16_t cond_flag = (instr >> 9) & 0x7;
+    if (cond_flag & reg[R_COND])
+    {
+        reg[R_PC] += pc_offset;
+    }
+}
+
+void JUMP()
+{
+    /* Also handles RET */
+    uint16_t r1 = (instr >> 6) & 0x7;
+    reg[R_PC] = reg[r1];
+}
+
+void JUMP_REGISTER()
+{
+    uint16_t long_flag = (instr >> 11) & 1;
+    reg[R_R7] = reg[R_PC];
+    if (long_flag)
+    {
+        uint16_t long_pc_offset = sign_extend(instr & 0x7FF, 11);
+        reg[R_PC] += long_pc_offset;  /* JSR */
+    }
+    else
+    {
+        uint16_t r1 = (instr >> 6) & 0x7;
+        reg[R_PC] = reg[r1]; /* JSRR */
+    }
+    break;
+}
+
+void LOAD()
+{
+    uint16_t r0 = (instr >> 9) & 0x7;
+    uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+    reg[r0] = mem_read(reg[R_PC] + pc_offset);
+    update_flags(r0);
+}
